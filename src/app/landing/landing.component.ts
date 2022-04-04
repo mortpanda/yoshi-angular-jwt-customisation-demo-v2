5a4d71f72af1417820dd5e669cb70e559a418713
Kent Nagao <31921593+mortpanda@users.noreply.github.com>
2022-04-04 12:55:19 +0900

    main
    main

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -18,29 +18,8 @@ export class LandingComponent implements OnInit {
 
   async ngOnInit()  {
     this.OktaWidgetService.CloseWidget();
+    this.OktaWidgetService.login();
     
-    this.strUserSession = await this.authService.session.exists()
-      .then(function (exists) {
-        if (exists) {
-          // logged in
-          console.log('Session to Okta : ' + exists);
-          return exists
-        } else {
-          // not logged in
-          console.log('Session to Okta : ' + exists);
-          return exists
-        }
-      });
-    switch (this.strUserSession) {
-      case false:
-        this.OktaWidgetService.login();
-        break;
-
-      case true:
-        window.location.replace(this.OktaSDKAuthService.config.redirectUri);
-        break;
-
-    }
   }
 
 }
